4aff38f77998443e55165fe58eb3441cf795c5cd
devmezcode <hermeskii01@gmail.com>
2025-06-30 04:50:56 -0400

    added function to auth

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,25 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if(!req.session.authorization) {
+        return res.status(403).json({message: "User not logged in"});
+    }
+
+    const token = req.session.authorization.accessToken;
+
+    if(!token) {
+        return res.status(403).json({message: "Access token missing"});
+    }
+
+    try {
+        const decoded = jwt.verify(token, "fingerprint_customer");
+
+        req.user = decoded;
+
+        next();
+    } catch(err) {
+        return res.status(403).json({message: "Invalid token"});
+    }
 });
  
 const PORT =5000;
