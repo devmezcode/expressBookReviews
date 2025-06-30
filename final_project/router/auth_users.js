499b3d017e92448763b25d163e95ade879494c9a
devmezcode <hermeskii01@gmail.com>
2025-06-30 05:37:01 -0400

    Cleaned up code, pausing and adding modules

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -16,13 +16,13 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
 });
 
 module.exports.authenticated = regd_users;
